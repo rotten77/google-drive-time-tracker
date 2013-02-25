5836cdb60ad623825412f327f713d7b72404e2e2
Jan Zatloukal <zatloukal.jan@gmail.com>
2013-02-25 11:45:20 +0100

    First version
    It works :-)

---
+++ ajax.php
@@ -0,0 +1,88 @@
+<?php
+$taskId = "entry_299071204";
+$categoryId = "entry_920947763";
+$tagId = "entry_662564008";
+$timeId = "entry_64794649";
+
+
+/**
+ * Data
+ */
+if(isset($_GET['get_data'])) {
+	$categories = array();
+	$tags = array();
+	/**
+	 * HTML of Form
+	 */
+	$ch = curl_init();
+	$googleForm = "https://docs.google.com/forms/d/".$_GET['form_id']."/viewform";
+	curl_setopt($ch, CURLOPT_URL, $googleForm);
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	$htmlForm = curl_exec($ch);
+	curl_close($ch);
+
+
+	/**
+	 * Get <select> box options
+	 */
+	$dom = new DOMDocument();
+	$dom->loadHTML($htmlForm);
+	$xpath = new DOMXPath($dom);
+	$htmlCategories = $xpath->evaluate("//select[@id='$categoryId']//option");
+	foreach($htmlCategories as $category) {
+		if($category->nodeValue!="") $categories[] = $category->nodeValue;
+	}
+	$htmlTags = $xpath->evaluate("//select[@id='$tagId']//option");
+	foreach($htmlTags as $tag) {
+		if($tag->nodeValue!="") $tags[] = $tag->nodeValue;
+	}
+
+	$return = array(
+			"categories" => $categories,
+			"tags" => $tags,
+		);
+
+	header('Cache-Control: no-cache, must-revalidate');
+	header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+	header('Content-type: application/json');
+
+	echo json_encode($return);
+}
+
+
+
+/**
+ * Data sending
+ */
+if(isset($_POST['send_data'])) {
+
+	$googleFormResponse = $googleForm = "https://docs.google.com/forms/d/".$_POST['form_id']."/formResponse";
+
+	$task = isset($_POST['task']) ? $_POST['task'] : "";
+	$category = isset($_POST['category']) ? $_POST['category'] : "";
+	$tag = isset($_POST['tag']) ? $_POST['tag'] : "";
+	$time = isset($_POST['time']) ? $_POST['time'] : "";
+
+	$taskId = str_replace("_", ".", $taskId);
+	$categoryId = str_replace("_", ".", $categoryId);
+	$tagId = str_replace("_", ".", $tagId);
+	$timeId = str_replace("_", ".", $timeId);
+
+	$postData = array(
+			$taskId => $task,
+			$categoryId => $category,
+			$tagId => $tag,
+			$timeId => $time,
+			"submit" => true
+		);
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $googleFormResponse);
+	curl_setopt($ch, CURLOPT_HEADER, 0); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+	curl_exec($ch);
+}
---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+	<meta charset="utf-8">
+	<title>Google Drive Time Tracker</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="./bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" />
+	<style>form {margin-top:12px;}</style>
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-static-top">
+	<div class="navbar-inner">
+		
+		<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+
+
+			<span class="brand">Time Tracker</span>
+			
+			<div class="nav-collapse collapse">
+				<ul class="nav">
+					<li><a href="./" id="change_id">Change form ID</a></li>
+					<li><a href="https://github.com/rotten77/google-drive-time-tracker" target="_blank">GitHub</a></li>
+				</ul>
+			</div>
+		</div>
+
+	</div>
+</div>
+<div class="container" id="timetracker">
+	<div class="row">
+		<form class="form-horizontal" method="post" id="form" action="./">
+			<div class="control-group" id="task_group">
+				<label class="control-label" for="task">Task</label>
+				<div class="controls">
+					<input type="text" id="task" name="task" placeholder="What are you working on?" class="input-xxlarge" />
+					<span class="help-inline" id="task_err">Enter task description</span>
+				</div>
+			</div>
+
+			<div class="control-group">
+				<label class="control-label" for="category">Category</label>
+				<div class="controls">
+					<select name="category" id="category"></select>
+				</div>
+			</div>
+
+			<div class="control-group">
+				<label class="control-label" for="tag">Tag</label>
+				<div class="controls">
+					<select name="tag" id="tag"></select>
+				</div>
+			</div>
+
+			<div class="control-group">
+				<div class="controls">
+
+
+			<div class="alert alert-info" id="working_from">
+				Working from: <strong id="working_from_value">#</strong>
+			</div>
+
+			<div class="alert alert-info" id="time_spend">
+				Time spend: <strong id="time_spend_value">#</strong>
+				<input name="time" id="time" type="hidden" value="">
+			</div>
+			
+					<button type="submit" name="send_data" id="send_data" class="btn btn-primary">Send</button>
+					<button class="btn btn-primary" id="start">Start</button>
+					<button class="btn btn-primary" id="stop">Stop</button>
+					<button class="btn btn-warning" id="cancel">Cancel</button>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
+
+<div class="container" id="settings">
+		<h2>Step 1: Make a form</h2>
+		<p>Create copy of this form...</p>
+		
+		<hr />
+		<h2>Step 2: Enter your form ID</h2>
+		<form class="form-horizontal" method="post" id="settings" action="./">
+			<div class="control-group" id="settings_group">
+				<label class="control-label" for="form_id">Form ID</label>
+				<div class="controls">
+					<input type="text" id="form_id" name="form_id" placeholder="Enter your Google Drive form ID" class="input-xxlarge" />
+					<span class="help-inline" id="form_id_err">Enter your Google Drive form ID</span>
+				</div>
+			</div>
+
+			<div class="control-group">
+				<div class="controls">
+					<button type="submit" name="send_data" id="save" class="btn btn-primary">Save</button>
+				</div>
+			</div>
+		</form>
+</div>
+
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="./bootstrap/js/bootstrap.min.js"></script>
+	<script>
+		$(function(){
+
+			var form_id = localStorage.getItem("form_id");
+
+			if(form_id=="" || form_id==null) {
+				$('#timetracker').hide();
+				$('#settings').show();
+			} else {
+				$('#timetracker').show();
+				$('#settings').hide();
+
+					$('#task_err, #cancel').hide();
+
+					$('#task').keydown(function(){
+						$('#task_group').removeClass("error");
+						$('#task_err').hide();
+					});
+
+					$.getJSON('./ajax.php?get_data=1&form_id='+form_id, function(data) {
+
+						$.each(data['categories'], function(key, val) {
+							var lastCategory = localStorage.getItem("lastCategory");
+							$("#category").append('<option value="'+val+'"'+(lastCategory==val ? ' selected="selected"' : '')+'>'+val+'</option>');
+						});
+
+						$.each(data['tags'], function(key, val) {
+							var lastCategory = localStorage.getItem("lastTag");
+							$("#tag").append('<option value="'+val+'"'+(lastCategory==val ? ' selected="selected"' : '')+'>'+val+'</option>');
+						});
+						
+		  			
+					});
+
+					$('#task').val(localStorage.getItem("lastTask"));
+
+					$('#tag, #category').change(function(){
+						localStorage.setItem("lastTag", $('#tag option:selected').val());
+						localStorage.setItem("lastCategory", $('#category option:selected').val());
+					});
+					
+
+					if(localStorage.getItem("lastTag")!="") {
+						//$('#tag option').removeAttr("selected");
+						//$('#tag option[value="'+localStorage.getItem("lastTag")+'"]').attr("selected", "selected");
+					}
+
+					if(localStorage.getItem("lastCategory")!="") {
+						//$('#category option').removeAttr("selected");
+						//$('#category option[value="'+localStorage.getItem("lastCategory")+'"]').attr("selected", "selected");
+					}
+
+					//$('#tag').change();
+
+					function returnTime() {
+						var d = new Date();
+						var n = d.getTime();
+
+						return n;
+					}
+
+					$('#form').submit(function(){
+						return false;
+					});
+					$('#send_data').click(function(){
+						if($('#task').val()=="") {
+							$('#task_group').addClass("error");
+							$('#task_err').show();
+							return false;
+						}
+						$.post("index.php", {
+								task: $('#task').val(),
+								category: $('#category option:selected').val(),
+								tag: $('#tag option:selected').val(),
+								time: $('#time').val(),
+								send_data: true
+							});
+
+									$('#task').val("");
+									$('#stop, #send_data, #working_from,#time_spend').hide();
+									$('#start').show();
+									localStorage.setItem("lastTask", "");
+							
+						return false;
+					});
+
+
+					
+					var lastTime = localStorage.getItem("lastTime")*1;
+					if(lastTime>0) {
+						$('#start, #send_data,#time_spend').hide();
+						$('#stop, #working_from').show();
+
+						var d = new Date(lastTime);
+						$('#working_from_value').text((d.getHours()<10 ? '0' : '')+d.getHours()+":"+(d.getMinutes()<10 ? '0' : '')+d.getMinutes());
+					} else {
+						$('#stop, #send_data, #working_from,#time_spend').hide();
+						$('#start').show();
+					}
+
+					$('#start').click(function(){
+						var actualTime = returnTime();
+						var d = new Date(actualTime);
+
+						localStorage.setItem("lastTime", actualTime);
+						localStorage.setItem("lastTask", $('#task').val());
+						$('#start, #send_data,#time_spend,#cancel').hide();
+						$('#stop, #working_from').show();
+						$('#working_from_value').text((d.getHours()<10 ? '0' : '')+d.getHours()+":"+(d.getMinutes()<10 ? '0' : '')+d.getMinutes());
+					});
+					$('#stop').click(function(){
+						var lastTime = localStorage.getItem("lastTime")*1;
+						// console.log(returnTime());
+						// console.log(lastTime);
+
+						var timeSpend = (returnTime()-lastTime)/60/60/1000;
+
+						timeSpend = Math.round(timeSpend*100)/100;
+						//timeSpend.replace(".", ",");
+						localStorage.setItem("lastTime", 0);
+
+						$('#time').val(timeSpend);
+						$('#time_spend_value').text(timeSpend+" hrs.");
+
+						$('#start, #stop, #working_from').hide();
+						$('#send_data,#time_spend,#cancel').show();
+					});
+					$('#cancel').click(function(){
+						$('#stop, #time_spend, #start, #send_data, #cancel').hide();
+						$('#start').show();
+						localStorage.setItem("lastTime", 0);
+					});
+			}
+
+
+			$('#form_id_err').hide();
+			$('#settings').submit(function(){
+				if($('#form_id').val()=="") {
+					$('#settings_group').addClass("error");
+					$('#form_id_err').show();
+					return false;
+				} else {
+					localStorage.setItem("form_id", $('#form_id').val());
+				}
+			});
+
+			$('#form_id').keydown(function(){
+				$('#settings_group').removeClass("error");
+				$('#form_id_err').hide();
+			});
+
+			$('#change_id').click(function(){
+				localStorage.setItem("form_id", "");
+			});
+					
+		});
+	</script>
+</body>
+</html>
